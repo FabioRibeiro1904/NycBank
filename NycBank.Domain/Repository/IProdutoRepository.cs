ba4db657e007099539ad0706a0dcdafd96e48ab2
Fabio Lucio Ribeiro <fabioribeiro1904@outlook.com>
2021-06-17 19:53:09 -0300

    Interface Repository Produto

---
+++ NycBank.Domain/Repository/IProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using NycBank.Domain.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace NycBank.Domain.Repository
+{
+    public interface IProdutoRepository
+    {
+        void Create(Produto produto);
+
+        IEnumerable<Produto> GetList();
+
+        Produto GetId(Guid id);
+
+        void Update(Produto produto);
+
+        void Delete(Guid id);
+    }
+}
