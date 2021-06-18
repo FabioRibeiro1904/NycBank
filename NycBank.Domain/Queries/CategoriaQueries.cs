b1a5543df7636faed33ab6ccd68fdc7caf7aa545
Fabio Lucio Ribeiro <fabioribeiro1904@outlook.com>
2021-06-17 21:20:58 -0300

    Criação de Queries

---
+++ NycBank.Domain/Queries/CategoriaQueries.cs
@@ -0,0 +1,15 @@
+﻿using NycBank.Domain.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace NycBank.Domain.Queries
+{
+    public class CategoriaQueries
+    {
+
+        public static Expression<Func<Categoria, bool>> GetName(string nome)
+        {
+            return x => x.Nome == nome;
+        }
+    }
+}
---
+++ NycBank.Domain/Queries/ProdutoQueries.cs
@@ -0,0 +1,14 @@
+﻿using NycBank.Domain.Entities;
+using System;
+using System.Linq.Expressions;
+
+namespace NycBank.Domain.Queries
+{
+    public static class ProdutoQueries
+    {
+        public static Expression<Func<Produto, bool>> GetName(string nome)
+        {
+            return x => x.Nome == nome;
+        }
+    }
+}
